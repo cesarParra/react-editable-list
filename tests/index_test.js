5f511e9c50a4b4e23695eabeeb2408e54e750b61
Cesar Parra <cesar.parra@gmail.com>
2018-08-18 17:46:09 -0400

    Unit tests

--- tests/index_test.js
+++ tests/index_test.js
@@ -6,8 +6,20 @@ import EditableList from 'src/';
 import ListItem from 'src/ListItem';
 
 describe('<EditableList />', () => {
-  it('renders an editable list', () => {
-    const editableList = mount(<EditableList title="Demo" items={['test1', 'test2']} itemPlaceholder="+" />);
+  it('allows us to set the title', () => {
+    const editableList = mount(<EditableList title="Test" />);
+    expect(editableList.props().title).to.equal('Test');
+    editableList.setProps({ title: 'Different Title' });
+    expect(editableList.props().title).to.equal('Different Title');
+  });
+
+  it('renders one item when no items are passed', () => {
+    const editableList = mount(<EditableList title="Test" itemPlaceholder="+" />);
+    expect(editableList.find(ListItem)).to.have.length(1);
+  });
+
+  it('renders correct number of list items when items are passed', () => {
+    const editableList = mount(<EditableList title="Test" items={['test1', 'test2']} itemPlaceholder="+" />);
     expect(editableList.find(ListItem)).to.have.length(3);
   });
 });
