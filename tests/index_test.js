979b1daf2a01fb32bd5fc3dffad1dd958416d1c5
Cesar Parra <cesar.parra@gmail.com>
2018-08-18 17:35:28 -0400

    Unit testing

--- tests/index_test.js
+++ tests/index_test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
 
 import EditableList from 'src/';
 import ListItem from 'src/ListItem';
 
 describe('<EditableList />', () => {
   it('renders an editable list', () => {
-    const editableList = shallow(<EditableList title="Demo" items={['test1', 'test2']} itemPlaceholder="+" />);
+    const editableList = mount(<EditableList title="Demo" items={['test1', 'test2']} itemPlaceholder="+" />);
     expect(editableList.find(ListItem)).to.have.length(3);
   });
 });
